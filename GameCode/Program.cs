d9cc1b10987f4c450da11407d363bc7b3acb97c6
josephgm2000 <61472269+josephgm2000@users.noreply.github.com>
2020-04-06 17:24:19 -0400

    Add project files.

---
+++ GameCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
