a2580d5fcd5115a539093d52b9271c0050596bd8
josephgm2000 <61472269+josephgm2000@users.noreply.github.com>
2020-04-07 14:07:52 -0400

    Created Player class

---
+++ GameCode/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameCode
+{
+    class Player
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Player(string firstName, string lastName)
+        {
+
+        }
+    }
+}
