6fcdf0d38300a680e06911857cf17ea20ec322b6
josephgm2000 <61472269+josephgm2000@users.noreply.github.com>
2020-04-07 14:48:31 -0400

    Worked on GoldenDome() method in the PowerUp class

---
+++ GameCode/PowerUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameCode
+{
+    public class PowerUp
+    {
+        public int MinesFlagged { get; set; }
+        public PowerUp(int minesFlagged)
+        {
+
+        }
+        public void GoldenDome(TimeSpan time)
+        {
+            if (MinesFlagged == .75)
+            {
+                time = TimeSpan.FromSeconds(45);
+                if (!(time == TimeSpan.FromSeconds(0)))
+                {
+
+                }
+            }
+        }
+    }
+}
